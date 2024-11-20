94b0f1c45f1d0d6039f14dd64b7f3a85d98514af
Anastasiia <a.khrypko.work@gmail.com>
2024-11-20 21:21:33 +0100

    skills changed

--- src/components/content/Skills.comp.jsx
+++ src/components/content/Skills.comp.jsx
@@ -1,31 +1,30 @@
 import { useEffect } from "react";
 
 const skillsArray = [
-    { title: "HTML 5", icon: 'fa-brands fa-html5' },
-    { title: "CSS 3", icon: 'fa-brands fa-css3-alt' },
-    { title: ["JavaScript", "(ES6)"], icon: 'fa-brands fa-js' },
-    { title: ["Sass", "(Scss)"], icon: 'fa-brands fa-sass' },
-    { title: "React.js", icon: 'fa-brands fa-react' },
-    { title: "Node.js", icon: 'fa-brands fa-node-js' },
-    { title: ["Bootstrap", "Tailwind"], icon: 'fa-brands fa-bootstrap' },
-    { title: "Responsive", icon: 'fa-solid fa-crop' },
-    { title: "Figma", icon: 'fa-brands fa-figma' },
-    { title: "Terminal", icon: 'fa-solid fa-terminal' },
-    { title: "Git", icon: 'fa-brands fa-git-alt' },
-    { title: "GitHub", icon: 'fa-brands fa-github' },
+    { title: "HTML 5", icon: "fa-brands fa-html5" },
+    { title: "CSS 3", icon: "fa-brands fa-css3-alt" },
+    { title: ["JavaScript", "(ES6)"], icon: "fa-brands fa-js" },
+    { title: ["Sass", "(Scss)"], icon: "fa-brands fa-sass" },
+    { title: "React", icon: "fa-brands fa-react" },
+    { title: "Node.js", icon: "fa-brands fa-node-js" },
+    { title: ["Bootstrap", "Tailwind"], icon: "fa-brands fa-bootstrap" },
+    { title: "Responsive", icon: "fa-solid fa-crop" },
+    { title: "Figma", icon: "fa-brands fa-figma" },
+    { title: "Terminal", icon: "fa-solid fa-terminal" },
+    { title: "Git", icon: "fa-brands fa-git-alt" },
+    { title: "GitHub", icon: "fa-brands fa-github" },
 ];
 
 const SkillsComponent = () => {
-
     useEffect(() => {
         const interval = setInterval(() => {
-            const skills = document.querySelectorAll('.skills-list li');
+            const skills = document.querySelectorAll(".skills-list li");
             const randomIndex = Math.floor(Math.random() * skillsArray.length);
             const randomSkill = skills[randomIndex];
-            randomSkill.classList.add('active');
+            randomSkill.classList.add("active");
 
             setTimeout(() => {
-                randomSkill.classList.remove('active');
+                randomSkill.classList.remove("active");
             }, 2000);
         }, 1000);
 
@@ -41,9 +40,8 @@ const SkillsComponent = () => {
                 <div className="skills-list">
                     <ul>
                         {skillsArray.map((skill, index) => (
-                            <li
-                                key={index}
-                            ><i className={skill.icon}></i>
+                            <li key={index}>
+                                <i className={skill.icon}></i>
                                 {Array.isArray(skill.title) ? (
                                     skill.title.map((t, i) => <p key={i}>{t}</p>)
                                 ) : (
@@ -57,7 +55,6 @@ const SkillsComponent = () => {
             <p className="background-text">My abilities</p>
         </section>
     );
-
 };
 
 export default SkillsComponent;
